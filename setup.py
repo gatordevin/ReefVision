275fb616f3b1f3fc0c9b4d5ecfd54b637debeab2
Danny Dasilva <yahchayildasilva@gmail.com>
2020-02-29 14:39:09 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,27 @@
-import setuptools
+from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="Orange_Vision",
-    version="0.0.1",
-    author="Danny Dasilva",
-    author_email="dannydasilva.solutions@gmail.com",
-    description="FRC Orange Vision",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/Danny-Dasilva/Orange_Vision",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
+setup(
+    name='Orange_Vision',
+    version="0.0.2",
+    description="FRC Orange Vision API",
+    long_description='API to run inference on image data coming from the camera.',
+    author='Danny Dasilva',
+    author_email='dannydasilva.solutions@gmail.com',
+    license='Apache 2',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'numpy>=1.12.1',
+        'Pillow>=4.0.0',
+        'pygobject>=3.22.0',
+        'protobuf>=3.0.0',
+        'edgetpu',
     ],
+    entry_points = {
+        'console_scripts': ['orange_classify=Orange_Vision.classify:main',
+                            'orange_classify_server=Orange_Vision.classify_server:main',
+                            'orange_detect=Orange_Vision.detect:main',
+                            'orange_detect_server=Orange_Vision.detect_server:main'],
+    },
+    python_requires='>=3.5.3',
 )
