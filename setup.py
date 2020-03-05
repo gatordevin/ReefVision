04c56e67878b147c8005e7d127bc7e120d11602f
Danny-Dasilva <yahchayildasilva@gmail.com>
2020-03-05 14:50:20 -0500

    set for release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Orange_Vision',
-    version="0.0.3",
+    version="0.0.5",
     description="FRC Orange Vision API",
     long_description='API to run inference on image data coming from the camera.',
     author='Danny Dasilva',
