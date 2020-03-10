b485f34d4f2c34dbb0ce590dc55d1f6b381db3eb
Danny-Dasilva <yahchayildasilva@gmail.com>
2020-03-10 16:54:09 +0000

    wifi search

--- Orange_Vision/streaming/helpers/old_wifi.py
+++ Orange_Vision/streaming/helpers/old_wifi.py
@@ -76,31 +76,54 @@ def connect_wifi(name, password=None):
     #         return request_password()
     #     return True
     # 
-def search():
-    pass
 
-def read_network_list():
-    pass
-def save_network_list():
-    pass
-def read_network():
-    #nmcli d
-    pass
-def ask_password():
-    #socket promt for password
-    pass
 
-if __name__ == '__main__':
-    wifi_list = search_wifi()
-    print(wifi_list)
-    # if connection['connection'] == False:
-    #     wifi = next(iter(wifi_list))
-    #     print(wifi, "no connect")
-    #     val = connect(str(wifi))
-    #     if val == False:
-    #         connect(str(wifi), 'orangejet')
-    # else:
-    #     wifi = next(iter(wifi_list))
-    #     print("connedcted")
-    #     disconnect(wifi)
-    # disconnect(connect[True])
+
+class Finder:
+    def __init__(self, *args, **kwargs):
+        self.server_name = kwargs['server_name']
+        self.password = kwargs['password']
+        self.interface_name = kwargs['interface']
+        self.main_dict = {}
+
+    def run(self):
+
+        os.system(f'sudo rm -rf /etc/NetworkManager/system-connections/*{self.server_name}*')
+        command = """sudo iwlist wlan0 scan | grep -ioE 'ssid:"(.*{}.*)'"""
+        result = os.popen(command.format(self.server_name))
+        result = list(result)
+        if "Device or resource busy" in result:
+                return None
+        else:
+            ssid_list = [item.lstrip('SSID:').strip('"\n') for item in result]
+            print("Successfully get ssids {}".format(str(ssid_list)))
+
+        for name in ssid_list:
+            try:
+                result = self.connection(name)
+            except Exception as exp:
+                print("Couldn't connect to name : {}. {}".format(name, exp))
+            else:
+                if result:
+                    print("Successfully connected to {}".format(name))
+
+    def connection(self, name):
+        try:
+            os.system("nmcli d wifi connect {} password {} iface {}".format(name,
+       self.password,
+       self.interface_name))
+        except:
+            raise
+        else:
+            return True
+
+if __name__ == "__main__":
+    # Server_name is a case insensitive string, and/or regex pattern which demonstrates
+    # the name of targeted WIFI device or a unique part of it.
+    server_name = "Tech-Garage6-5G"
+    password = "techgrocks"
+    interface_name = "wlan0" # i. e wlp2s0  
+    F = Finder(server_name=server_name,
+               password=password,
+               interface=interface_name)
+    F.run()
