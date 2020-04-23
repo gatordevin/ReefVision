28147e2e6230aba0d52da2aeb485480c0807928a
gatordevin <gatordevin@gmail.com>
2020-04-23 13:49:30 -0400

    Added video flip to H264

--- Reef_Vision/CameraManager/TPUCameraManager.py
+++ Reef_Vision/CameraManager/TPUCameraManager.py
@@ -104,8 +104,8 @@ class StreamValue():
 
 class GStreamerPipelines(enum.Enum):
     SRC = "v4l2src device=/dev/video{0} ! tee name=t"
-    H264 = "t. ! queue max-size-buffers=1 leaky=downstream ! video/x-raw,format=YUY2,width={0},height={1},framerate={2}/1 ! videoconvert ! x264enc speed-preset=ultrafast tune=zerolatency threads=4 key-int-max=5 bitrate=1000 aud=False bframes=1 ! video/x-h264,profile=baseline ! h264parse ! video/x-h264,stream-format=byte-stream,alignment=nal ! appsink name={3} emit-signals=True max-buffers=1 drop=False sync=False"
-    RGB = "t. ! queue ! glfilterbin filter=glbox ! video/x-raw,format=RGB,width={0},height={1},framerate={2}/1 ! ! videoflip method=clockwise ! appsink name={3} emit-signals=True max-buffers=1 drop=True sync=False"
+    H264 = "t. ! queue max-size-buffers=1 leaky=downstream ! video/x-raw,format=YUY2,width={0},height={1},framerate={2}/1 ! videoflip method=clockwise ! videoconvert ! x264enc speed-preset=ultrafast tune=zerolatency threads=4 key-int-max=5 bitrate=1000 aud=False bframes=1 ! video/x-h264,profile=baseline ! h264parse ! video/x-h264,stream-format=byte-stream,alignment=nal ! appsink name={3} emit-signals=True max-buffers=1 drop=False sync=False"
+    RGB = "t. ! queue ! glfilterbin filter=glbox ! video/x-raw,format=RGB,width={0},height={1},framerate={2}/1 ! videoflip method=clockwise ! appsink name={3} emit-signals=True max-buffers=1 drop=True sync=False"
     MJPEG = "t. ! queue ! video/x-raw,format=YUY2,width={0},height={1},framerate={2}/1 ! jpegenc quality=20 ! appsink name={3} emit-signals=True"
 
     def __str__(self):
